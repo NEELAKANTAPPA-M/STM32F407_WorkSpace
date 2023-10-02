e94a9ce5a4f765ed868869cdde3bf05fb2d9509e
Neelakantappa M <neelakantamaagi253@gmail.com>
2023-10-02 23:31:41 +0530

    [F407]: Added baremetal project
    This project involves openOCD to attach, load & run the binary to the
    target.
    No need of any IDEs/
    Need to install openOCD
    
    Make file support is provided to compile the binary & to start the
    openOCD
    
    Signed-off-by: Neelakantappa M <neelakantamaagi253@gmail.com>

---
+++ 015_task_scheduler_bare_metal/type.h
@@ -0,0 +1,18 @@
+/*
+ * type.h
+ *
+ *  Created on: 07-May-2023
+ *      Author: neelakanta
+ */
+
+#ifndef TYPE_H_
+#define TYPE_H_
+
+typedef unsigned int u32;
+typedef unsigned short int u16;
+typedef unsigned char u8;
+typedef signed int s32;
+typedef signed short int s16;
+typedef signed char s8;
+
+#endif /* TYPE_H_ */
